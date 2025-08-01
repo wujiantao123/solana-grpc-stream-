21d876125d3c36fb58b481261c783461a547afcf
知无涯 <wujiantao1005@gmail.com>
2025-08-01 23:45:22 +0800

    test

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 import { subscribe, CommitmentLevel, LaserstreamConfig, SubscribeRequest } from 'helius-laserstream';
-
+import bs58 from 'bs58';
+import { Connection, PublicKey } from '@solana/web3.js';
+const connection = new Connection("https://mainnet.helius-rpc.com/?api-key=c64adbb9-8f0e-48b5-8690-a4d8bb4e5486", "confirmed");
 async function main() {
   const subscriptionRequest: SubscribeRequest = {
     transactions: {
       client: {
-        accountInclude: ['TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'],
+        accountInclude: ['BjuD62v9RysrburpKb65UKeaAWRSFyi7pFLLxdE3dPv'],
         accountExclude: [],
         accountRequired: [],
         vote: false,
@@ -23,11 +25,34 @@ async function main() {
 
   const config: LaserstreamConfig = {
     apiKey: '',
-    endpoint: '84.32.103.140:10040',
+    endpoint: 'http://84.32.103.140:10040',
+  }
+
+  const getAddressTransfer = async (address: string) => {
+    const signatures = await connection.getSignaturesForAddress(new PublicKey(address), { limit: 1 });
+    signatures.forEach(async (signature) => {
+      const result = await connection.getParsedTransaction(signature.signature, {
+        maxSupportedTransactionVersion: 0
+      });
+      if (result) {
+        const accountKeys = result.transaction.message.accountKeys.map(item => item.pubkey.toBase58());
+        const closeBalance = result.meta ? result.meta.preBalances[1] / 10 ** 9 : 0;
+        console.log(`transfer ${address} -> ${accountKeys[3]}`, closeBalance);
+      }
+    });
   }
 
   await subscribe(config, subscriptionRequest, async (data) => {
-    console.log(data);
+    const result = data.transaction
+    if(result){
+      const signature = bs58.encode(result.transaction.signature);
+      const accountKeys = result.transaction.transaction.message.accountKeys.map((buffer:Uint8Array | number[]) => bs58.encode(buffer));
+      const searchAccount = accountKeys[1]
+      const preBalances = result.meta ? result.meta.preBalances : [];
+      const balance = preBalances[1] / 10 ** 9;
+      console.log(`signature: ${signature}, account: ${searchAccount}, balance: ${balance}`);
+      getAddressTransfer(searchAccount);
+    }
   }, async (error) => {
     console.error(error);
   });
