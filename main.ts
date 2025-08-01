56e063b7e8e5c6938b85d9567fb0ebe0e3c4b435
知无涯 <wujiantao1005@gmail.com>
2025-08-02 00:31:05 +0800

    fix

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import bs58 from 'bs58';
 import { Connection, PublicKey } from '@solana/web3.js';
 const connection = new Connection("https://mainnet.helius-rpc.com/?api-key=c64adbb9-8f0e-48b5-8690-a4d8bb4e5486", "confirmed");
 const getAddressTransfer = async (address: string) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
     const signatures = await connection.getSignaturesForAddress(new PublicKey(address), { limit: 1 });
     signatures.forEach(async (signature) => {
       const result = await connection.getParsedTransaction(signature.signature, {
@@ -11,7 +12,7 @@ const getAddressTransfer = async (address: string) => {
       if (result) {
         const accountKeys = result.transaction.message.accountKeys.map(item => item.pubkey.toBase58());
         const closeBalance = result.meta ? result.meta.preBalances[1] / 10 ** 9 : 0;
-        console.log(`transfer ${address} -> ${accountKeys[2]}`, closeBalance,signature.signature);
+        console.log(`transfer ${address} -> ${accountKeys[3]}`, closeBalance,signature.signature);
       }
     });
   }
@@ -45,16 +46,12 @@ async function main() {
   await subscribe(config, subscriptionRequest, async (data) => {
     const result = data.transaction
     if(result){
-      const signature = bs58.encode(result.transaction.signature);
+      // const signature = bs58.encode(result.transaction.signature);
       const accountKeys = result.transaction.transaction.message.accountKeys.map((buffer:Uint8Array | number[]) => bs58.encode(buffer));
       const searchAccount = accountKeys[1]
-      const preBalances = result.transaction.meta ? result.transaction.meta.preBalances : [];
-      const postBalances = result.transaction.meta ? result.transaction.meta.postBalances : [];
-      const balance = (postBalances[1] - preBalances[1]) / 10 ** 9;
-      console.log(`signature: ${signature}, account: ${searchAccount}, balance: ${balance}`);
-      if(balance>3&&balance<4){
-        getAddressTransfer(searchAccount);
-      }
+      // const preBalances = result.transaction.meta ? result.transaction.meta.preBalances : [];
+      // const postBalances = result.transaction.meta ? result.transaction.meta.postBalances : [];
+      getAddressTransfer(searchAccount);
     }
   }, async (error) => {
     console.error(error);
